580cb3b19aca92678259fec2f63752ca127ae481
uk-ar <yuuki.ari@gmail.com>
2014-10-08 02:07:29 +0900

    for trac

---
+++ run.sh
@@ -0,0 +1,15 @@
+# Host名
+export HUBOT_KANDAN_HOST=192.168.59.103
+# Portが80以外の場合
+export HUBOT_KANDAN_PORT=8080
+# 取得したアクセスキー
+export HUBOT_KANDAN_TOKEN=gj2WhUGA7CKXbZy67Vib
+
+export HUBOT_TRAC_URL=http://192.168.59.103:8081/trac
+export HUBOT_TRAC_USER=admin
+export HUBOT_TRAC_PASSWORD=admin
+export HUBOT_TRAC_JSONRPC=true
+export HUBOT_TRAC_SCRAPE=false
+
+./bin/hubot -a kandan
+
