39300d4b3711b259a8150d158eb3ee1ad094fb69
uk-ar <yuuki.ari@gmail.com>
2014-10-09 05:29:47 +0900

    for redmine

--- run.sh
+++ run.sh
@@ -14,6 +14,12 @@ export HUBOT_TRAC_SCRAPE=false
 export HUBOT_RSS_INTERVAL=60   # 60 sec
 export HUBOT_RSS_HEADER=:sushi: # RSS Header Emoji (default is "sushi")
 export DEBUG=hubot-rss-reader   # debug print
+# @hubot rss add http://192.168.59.103:8081/trac/timeline?format=rss
 
+#export HUBOT_REDMINE_SSL=""
+export HUBOT_REDMINE_BASE_URL="http://192.168.59.103:10080/"
+export HUBOT_REDMINE_TOKEN="844ef707e403cb14b187f2820f3bea6823b3e8cb"
+#export HUBOT_REDMINE_IGNORED_USERS=""
+#hubot rss add http://192.168.59.103:10080/projects/aa/activity.atom?key=dc6b646db751ac8eb9dd5de8f40041eb21212ed7
 ./bin/hubot -a kandan
 
